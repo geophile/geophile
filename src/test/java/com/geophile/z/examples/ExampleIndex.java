3c2ee1cde124da2d89af1d49b22c8c1436a90a8b
Jack Orenstein <jao@geophile.com>
2014-08-13 19:31:31 -0400

    Forgot a new file.

---
+++ src/test/java/com/geophile/z/examples/ExampleIndex.java
@@ -0,0 +1,23 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+package com.geophile.z.examples;
+
+import com.geophile.z.index.tree.TreeIndex;
+
+public class ExampleIndex extends TreeIndex<ExampleRecord>
+{
+    @Override
+    public ExampleRecord newRecord()
+    {
+        return new ExampleRecord();
+    }
+
+    public ExampleIndex()
+    {
+        super(ExampleRecord.COMPARATOR);
+    }
+}
